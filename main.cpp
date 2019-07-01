44faf06513be18645c388aa2fb77880f9cb45c70
dbecad <dbecadgit@gmail.com>
2019-06-30 18:58:33 -0700

    Implementing PtrDetails class and tests

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
-#include "gc_pointer.h"
-#include "LeakTester.h"
+#include <iostream>
+using namespace std;
+
+
+#include "gc_details.h"
+// #include "gc_pointer.h"
+// #include "LeakTester.h"
 
 int main()
 {
-    Pointer<int> p = new int(19);
-    p = new int(21);
-    p = new int(28);
+    //Test Pointer details
+    testPtrDetails();
 
+    //Commented for now
+    // Pointer<int> p = new int(19);
+    // p = new int(21);
+    // p = new int(28);
+
+    cout << "Done!" << endl;   
     return 0;
 }
